d819e91a29996adb5251b4d036f2a92f00be9fd3
plasticuproject <nicholes.bowling@gmail.com>
2018-07-24 12:44:08 -0400

    Update session_chat.py

--- examples/session_chat.py
+++ examples/session_chat.py
@@ -4,28 +4,30 @@ chat session untill closed.
 '''
 
 import cleverbotfree.cbfree
+import sys
 
 cb = cleverbotfree.cbfree.Cleverbot()
 
 
 def main():
-
     try:
-        print('[-] Connecting to Cleverbot.com...\n')
         cb.browser.get(cb.url)
-        print('[-] Type "quit" at any time to exit.\n')
-        while True:
+    except:
+        cb.browser.close()
+        sys.exit()
+    while True:
+        try:
             cb.get_form()
-            userInput = input('User: ')
-            if userInput == 'quit':
-                break
-            cb.send_input(userInput)
-            print('\nCleverbot: ' + cb.get_response() + '\n')
-        cb.browser.close()
-    except KeyboardInterrupt:
-        print('\nThanks for chatting!\n')
-        cb.browser.close()
+        except:
+            sys.exit()
+        userInput = input('User: ')
+        if userInput == 'quit':
+            break
+        cb.send_input(userInput)
+        bot = cb.get_response()
+        print('Cleverbot: ', bot)
+    cb.browser.close()
 
-
+    
 if __name__ == '__main__':
     main()
