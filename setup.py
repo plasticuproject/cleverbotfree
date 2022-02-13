e07c9537d7b49777c153698bc4a8dccc9eaa3565
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2022-02-13 12:05:03 -0500

    Cleaned up package a bit, added gitflow linting and pypi deployment

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-from setuptools import setup
+"""Setup File"""
+from setuptools import setup  # type: ignore
 
-version = '2.3.3'
+VERSION = "2.3.4"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 setup(
-    name='cleverbotfree',
-    packages=['cleverbotfree'],
-    version=version,
-    description='Free Alternative For The Cleverbot API',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='plasticuproject',
-    author_email='plasticuproject@pm.me',
-    url='https://github.com/plasticuproject/cleverbotfree',
-    download_url='https://github.com/plasticuproject/cleverbotfree/archive/v' +
-    version + '.tar.gz',
-    keywords=['cleverbot', 'bot', 'api', 'free'],
+    name="cleverbotfree",
+    packages=["cleverbotfree"],
+    version=VERSION,
+    description="Free Alternative For The Cleverbot API",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="plasticuproject",
+    author_email="plasticuproject@pm.me",
+    url="https://github.com/plasticuproject/cleverbotfree",
+    download_url="https://github.com/plasticuproject/cleverbotfree/archive/v" +
+    VERSION + ".tar.gz",
+    keywords=["cleverbot", "bot", "api", "free"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Communications :: Chat', 'Topic :: Utilities'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License" +
+        " v3 or later (GPLv3+)", "Programming Language :: Python :: 3",
+        "Topic :: Communications :: Chat", "Topic :: Utilities"
     ],
-    license='GPLv3',
-    install_requires=['playwright', 'fake-useragent'],
+    license="GPLv3",
+    install_requires=["playwright", "fake-useragent"],
     zip_safe=False,
     include_package_data=True)
